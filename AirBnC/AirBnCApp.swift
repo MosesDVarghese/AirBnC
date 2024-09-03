bec7ce9739be18884119db77d98d88fa0ba7930c
mosesdvarghese <accydent@gmail.com>
2024-09-03 20:30:58 +0400

    Initial Commit

---
+++ AirBnC/AirBnCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnCApp.swift
+//  AirBnC
+//
+//  Created by Moses Varghese on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
