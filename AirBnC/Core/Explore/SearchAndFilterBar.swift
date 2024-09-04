ceb0b718475c17552a59c651da7669230dd92ff0
mosesdvarghese <accydent@gmail.com>
2024-09-04 14:45:15 +0400

    add custom search bar

---
+++ AirBnC/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,46 @@
+//
+//  SearchAndFilterBar.swift
+//  AirBnC
+//
+//  Created by Moses Varghese on 9/4/24.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any week - Add guetsts")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button(action: {}, label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            })
+        }
+        .padding(.horizontal)
+        .padding(.vertical)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
