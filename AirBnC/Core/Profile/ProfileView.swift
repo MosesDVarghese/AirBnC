f50588e31b945f7be2ebfb9fded8c610a4c2ec11
mosesdvarghese <accydent@gmail.com>
2024-09-05 16:22:41 +0400

    add destination search view

---
+++ AirBnC/Core/Profile/ProfileView.swift
@@ -0,0 +1,62 @@
+//
+//  ProfileView.swift
+//  AirBnC
+//
+//  Created by Moses Varghese on 9/5/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            
+            // profile login view
+            VStack(alignment: .leading, spacing: 32) {
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Profile")
+                        .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    
+                    Text("Login to start planning your next trip")
+                }
+                
+                Button {
+                    print("Log in")
+                } label: {
+                    Text("Log in")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 48)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+                HStack {
+                    Text("Dont have an account?")
+                    
+                    Text("Sign up")
+                        .fontWeight(.semibold)
+                        .underline()
+                }
+                .font(.caption)
+            }
+            
+            VStack(spacing: 24) {
+                ProfileOptionRowView(imageName: "gear", title: "Settings")
+                
+                ProfileOptionRowView(imageName: "gear", title: "Accessibility")
+                
+                ProfileOptionRowView(imageName: "questionmark.circle", title: "Visit the help center")
+            }
+            .padding(.vertical)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileView()
+}
